4ac0bd68b39761f1917aa53642aa980382576454
Ivo Georgiev <ivo@linvo.org>
2014-02-23 17:47:28 +0200

    No more context hack; fixed directly in dnode-protocol

--- example/server.js
+++ example/server.js
@@ -9,9 +9,6 @@ LinvoIPC.defineService("sample", function(remote/*, conn*/)
     sample.zing = function (n, cb) { cb(n * 100, new Date()) };
     _.extend(sample, new EventEmitter());
     
-    // This needs to be worked-around; instead of passing a function and extending it's context,
-    // we should return a brand-new sevice object and find a way to automatically bind all it's methods to it's context
-    sample.on = sample.on.bind(sample);
     setInterval(function()    
     {
         sample.emit("test", "test str")
