5aea2645d1143f7dbd0ed918ca65c759ec368d73
Ivo Georgiev <ivo@linvo.org>
2014-02-23 21:04:57 +0200

    Basic framework for authentication (handshake for users on system services)

--- linvoipc.js
+++ linvoipc.js
@@ -2,11 +2,15 @@ var dnode = require("dnode");
 var path = require("path");
 var mkdirp = require("mkdirp");
 var fs = require("fs");
+var net = require("net");
 
 var systemSocketPath = "/var/run/linvo-ipc";
 var userSocketPath = path.join(process.env.HOME, ".linvo-ipc");
 mkdirp.sync(userSocketPath);
 
+/*
+ * Client
+ */
 var userServices = {},
     systemServices = {};
 
@@ -18,24 +22,44 @@ function fillServices(socketPath, services)
     {
         var serviceName = path.basename(socket,".lipc");
         services[serviceName] = function(cb) {
-            dnode.connect(path.join(socketPath, socket), function(remote, conn) { cb(remote.service) });
+            var d = dnode(), c = net.connect(path.join(socketPath, socket));
+            d.on("remote", function(remote) { cb(remote.service) });
+            c.pipe(d).pipe(c);
         }
     });
 }
 fillServices(userSocketPath, userServices);
 fillServices(systemSocketPath, systemServices);
 
+
+/*
+ * Server
+ */
+function authenticate(connection, ready)
+{
+    if (process.getuid() != 0)
+        return ready();
+    
+    console.log("TODO: authenticate");
+    ready();
+};
+
 function defineService(name, constructor, options)
 {
-    var server = dnode(function(remote, conn)
-    {
-        this.service = constructor(remote, conn);
-    }, options);
-    
     var socketPath = path.join(process.getuid() == 0 ? systemSocketPath : userSocketPath, name+".lipc");
     
     try { fs.unlinkSync(socketPath) } catch(e){ };
-    server.listen(socketPath);
+    net.createServer(function(c)
+    {
+        authenticate(c, function(user)
+        {
+            var d = dnode(function(remote, conn)
+            {
+                this.service = constructor(remote, conn, user);
+            }, options);
+            c.pipe(d).pipe(c);
+        });
+    }).listen(socketPath);
     if (process.getuid() == 0) fs.chmodSync(socketPath, "0766");
 }
 
